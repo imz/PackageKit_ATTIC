aaede0bc51a893d4c3f77d4809e06758b6a099c0
Aleksei Nikiforov <darktemplar@altlinux.org>
2018-06-15 18:21:42 +0300

    Remove debconf support and debconf-related stuff
    (cherry picked from commit 1.1.12-alt1~3
    4f162d329 (Remove debconf support, 2018-06-15)
    and squashed with the following changes:
    )
    
    Revert debconf-specific part of "trivial: apt: Address some minor style nitpicks"
    
    This revert is to make rebasing ALT's patches easier.
    
    This reverts commit 210a2e77588143c54c56285c872d27fe9ee39374.
    
    Revert (dpkg-specific) "aptcc: Detect if apt system settings specify --force-conf* options and if so do not override them"
    
    This reverts commit 897ab51f5aaca58a29f29ef90738e0d68770d4ee.
    
    APT: with ALT's apt-rpm, Dpkg::Options "--force-conf{def,old}" make no sense
    
    (cherry picked from commit 1.2.5.0.0.31-alt1~1
    e934ae34d (APT: with ALT's apt-rpm, Dpkg::Options "--force-conf{def,old}" make no sense, 2025-01-16)
    )
    
    Revert "aptcc: pk-debconf-helper: raise timeout to 60 seconds"
    
    This reverts commit 360a059597786ea62bde64d22953714be0e507bd.
    
    Revert "aptcc: Introduce socket-activated pk-debconf-helper"
    
    This reverts commit d57588f2b723e36c842c453810f81025285ef2bd.
    
    (cherry picked from commit 1.1.13-alt1~2
    2aa1dcfd9 (Revert "aptcc: Introduce socket-activated pk-debconf-helper", 2020-04-01)
    )

--- backends/apt/apt-job.h
+++ backends/apt/apt-job.h
@@ -247,7 +247,6 @@ private:
     bool packageIsSupported(const pkgCache::VerIterator &verIter, string component);
     bool isApplication(const pkgCache::VerIterator &verIter);
     bool matchesQueries(const vector<string> &queries, string s);
-    bool dpkgHasForceConfFileSet();
     PkInfoEnum packageStateFromVer(const pkgCache::VerIterator &ver) const;
     void stagePackageForEmit(GPtrArray *array, const pkgCache::VerIterator &ver,
                              PkInfoEnum state = PK_INFO_ENUM_UNKNOWN,
