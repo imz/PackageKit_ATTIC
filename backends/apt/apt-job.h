7629631bbd78ac2de1cfdbfc64a73dfca9391f15
Aleksei Nikiforov <darktemplar@altlinux.org>
2018-06-15 17:51:37 +0300

    Remove support of 'supported' check (and classification based on the origin)
    [imz: both the supported check and the classification (security/bugfix/etc)
    are based on the origin repository, i.e., "Debian-backports", "Debian-security",
    which are Debians-pecific etc.]
    
    (cherry picked from commit 0c82b3ce94f37a36d54fc0a33dc86de3db7abd19)

--- backends/apt/apt-job.h
+++ backends/apt/apt-job.h
@@ -233,7 +233,6 @@ public:
 
 private:
     void setEnvLocaleFromJob();
-    bool packageIsSupported(const pkgCache::VerIterator &verIter, string component);
     bool isApplication(const pkgCache::VerIterator &verIter);
     bool matchesQueries(const vector<string> &queries, string s);
 
