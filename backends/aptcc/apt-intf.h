1c1f753ea961fe3571e30b132610e0c505b7b7d6
Aleksei Nikiforov <darktemplar@altlinux.org>
2018-06-15 17:51:37 +0300

    Remove support of 'supported' check
    (cherry picked from commit 0c82b3ce94f37a36d54fc0a33dc86de3db7abd19)

--- backends/aptcc/apt-intf.h
+++ backends/aptcc/apt-intf.h
@@ -228,7 +228,6 @@ public:
 
 private:
     void setEnvLocaleFromJob();
-    bool packageIsSupported(const pkgCache::VerIterator &verIter, string component);
     bool isApplication(const pkgCache::VerIterator &verIter);
     bool matchesQueries(const vector<string> &queries, string s);
 
