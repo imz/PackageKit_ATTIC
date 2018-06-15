2f3e9fe3d080b1cf602bce66f6e980c9d0fb7e99
Aleksei Nikiforov <darktemplar@altlinux.org>
2018-06-15 17:51:37 +0300

    Remove support of 'supported' check
    (cherry picked from commit 0c82b3ce94f37a36d54fc0a33dc86de3db7abd19)

--- backends/aptcc/pk-backend-aptcc.cpp
+++ backends/aptcc/pk-backend-aptcc.cpp
@@ -127,7 +127,6 @@ PkBitfield pk_backend_get_filters(PkBackend *backend)
                 PK_FILTER_ENUM_GUI,
                 PK_FILTER_ENUM_INSTALLED,
                 PK_FILTER_ENUM_DEVELOPMENT,
-                PK_FILTER_ENUM_SUPPORTED,
                 PK_FILTER_ENUM_FREE,
                 PK_FILTER_ENUM_APPLICATION,
                 PK_FILTER_ENUM_DOWNLOADED,
