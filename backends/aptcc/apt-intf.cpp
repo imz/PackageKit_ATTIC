0d0e4f50a37be123ca5082af8339436b90c652c9
Aleksei Nikiforov <darktemplar@altlinux.org>
2018-06-19 11:37:10 +0300

    Implement searchPackageFiles (adaped for ALT/rpm)
    (cherry picked from commit ee5914e5926c005f1db2cea81241ae30191d157b)

--- backends/aptcc/apt-intf.cpp
+++ backends/aptcc/apt-intf.cpp
@@ -34,6 +34,7 @@
 #include <apt-pkg/algorithms.h>
 #include <apt-pkg/pkgsystem.h>
 #include <apt-pkg/version.h>
+#include <apt-pkg/rpmsystem.h>
 
 #include <appstream.h>
 
@@ -1187,7 +1188,7 @@ PkgList AptIntf::searchPackageDetails(const vector<string> &queries)
     return output;
 }
 
-// used to return files it reads, using the info from the files in /var/lib/dpkg/info/
+// used to return files it reads
 PkgList AptIntf::searchPackageFiles(gchar **values)
 {
     PkgList output;
@@ -1225,37 +1226,43 @@ PkgList AptIntf::searchPackageFiles(gchar **values)
         regfree(&re);
     } BOOST_SCOPE_EXIT_END;
 
-    DIR *dp;
-    struct dirent *dirp;
-    if (!(dp = opendir("/var/lib/dpkg/info/"))) {
-        g_debug ("Error opening /var/lib/dpkg/info/\n");
-        return output;
-    }
+    // Non-owner pointer
+    RPMDBHandler *db_handler = rpmSys.GetDBHandler();
 
-    string line;
-    while ((dirp = readdir(dp)) != NULL) {
+    for (Header header = db_handler->GetHeader(); header; db_handler->Skip(), header = db_handler->GetHeader())
+    {
         if (m_cancel) {
             break;
         }
 
-        if (ends_with(dirp->d_name, ".list")) {
-            string file(dirp->d_name);
-            string f = "/var/lib/dpkg/info/" + file;
-            ifstream in(f.c_str());
-            if (!in != 0) {
-                continue;
-            }
+        const char *FileName;
+        rpmtd fileNames = rpmtdNew();
 
-            while (!in.eof()) {
-                getline(in, line);
-                if (regexec(&re, line.c_str(), (size_t)0, NULL, 0) == 0) {
-                    packages.push_back(file.erase(file.size() - 5, file.size()));
-                    break;
+        BOOST_SCOPE_EXIT( (&fileNames) )
+        {
+            rpmtdFreeData(fileNames);
+            rpmtdFree(fileNames);
+        } BOOST_SCOPE_EXIT_END;
+
+        if ((headerGet(header, RPMTAG_OLDFILENAMES, fileNames, HEADERGET_EXT) != 1)
+            && (headerGet(header, RPMTAG_FILENAMES, fileNames, HEADERGET_EXT) != 1))
+        {
+            g_debug("Rpmdb processing error");
+            return output;
+        }
+
+        while ((FileName = rpmtdNextString(fileNames)) != NULL)
+        {
+            if (regexec(&re, FileName, (size_t)0, NULL, 0) == 0)
+            {
+                const char *packageName = headerGetString(header, RPMTAG_NAME);
+                if (packageName)
+                {
+                    packages.push_back(packageName);
                 }
             }
         }
     }
-    closedir(dp);
 
     // Resolve the package names now
     for (const string &name : packages) {
