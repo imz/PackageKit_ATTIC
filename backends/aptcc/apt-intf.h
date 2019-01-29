afa9fa640a9cce292ee90ec8f37ccaad0c50c8fd
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-01-29 11:37:45 +0300

    Update locking
    (cherry picked from commit 9b4d6ca3f0f3a4d4f45af4f6fd4cfddde65ede97)

--- backends/aptcc/apt-intf.h
+++ backends/aptcc/apt-intf.h
@@ -29,6 +29,7 @@
 
 #include <apt-pkg/depcache.h>
 #include <apt-pkg/acquire.h>
+#include <apt-pkg/fileutl.h>
 
 #include <pk-backend.h>
 
@@ -254,6 +255,8 @@ private:
     // when the internal terminal timesout after no activity
     int m_terminalTimeout;
     pid_t m_child_pid;
+
+    FileFd m_fileFd;
 };
 
 #endif
