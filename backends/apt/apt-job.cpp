39f70ac2e447e9c35fa96cb8f57ba2a6de760b02
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-06-26 13:50:12 +0300

    small adaptation of refreshCache (for ALT)
    [imz: The only substantial change from that commit:]
    
    AptCC: restore support for refreshCache action
    
    (cherry picked from commit 33cde34a031fd7092d04ddfb3cd8edcbc38d138c)

--- backends/apt/apt-job.cpp
+++ backends/apt/apt-job.cpp
@@ -1944,7 +1944,7 @@ void AptJob::refreshCache()
     AcqPackageKitStatus Stat(this);
 
     // do the work
-    ListUpdate(Stat, *m_cache->GetSourceList());
+    ListUpdate(Stat, *m_cache->GetSourceList(), *m_cache);
 
     // Rebuild the cache.
     pkgCacheFile::RemoveCaches();
