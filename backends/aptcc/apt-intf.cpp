436fd89d1fc14f2edebe4910b17abbd736eea595
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-08-29 18:06:22 +0300

    pkcon get-updates: list installed, removed and obsoleted packages [imz: diff ALT/upstream]
    This is a combination of 5 commits.
    
    pkcon get-updates: take into account Obsoletes
    
    (cherry picked from commit 2faafbb709019ef47b65f978dc9eebd61216cfea)
    
    get-updates: print removals
    
    update: still ignore removals, it should be handled automatically
    (cherry picked from commit e1e690c73a0a98d6789c2d4ff7c5456c998e8b88)
    
    pkcon get-updates: show obsoleted packages correctly
    
    (cherry picked from commit 10b52564c45f929bdfe5b585199ac58fc9137922)
    
    Fix invalid warning on empty update
    
    (cherry picked from commit 29e2d7d79d9e95338379314b7db7eaec56c423a9)
    
    Rework filtering of packages during update
    
    (cherry picked from commit df852207770eabdc802af54631dbd039f22f7442)

--- backends/aptcc/apt-intf.cpp
+++ backends/aptcc/apt-intf.cpp
@@ -1172,14 +1172,15 @@ PkgList AptIntf::getUpdates(PkgList &blocked, PkgList &downgrades, PkgList &inst
             if (!ver.end()) {
                 bool is_obsoleted = false;
 
+                /* Following code fragment should be similar to pkgDistUpgrade's one */
                 for (pkgCache::DepIterator D = pkg.RevDependsList(); not D.end(); ++D)
                 {
                     if ((D->Type == pkgCache::Dep::Obsoletes)
                         && ((*m_cache)[D.ParentPkg()].CandidateVer != nullptr)
                         && (*m_cache)[D.ParentPkg()].CandidateVerIter(*m_cache).Downloadable()
                         && ((pkgCache::Version*)D.ParentVer() == (*m_cache)[D.ParentPkg()].CandidateVer)
-                        && (*m_cache)->VS().CheckDep(pkg.CurrentVer().VerStr(), D->CompareOp, D.TargetVer())
-                        && ((*m_cache)->GetPolicy().GetPriority(D.ParentPkg()) >= (*m_cache)->GetPolicy().GetPriority(pkg)))
+                        && (*m_cache)->VS().CheckDep(pkg.CurrentVer().VerStr(), D)
+                        && ((*m_cache)->GetPkgPriority(D.ParentPkg()) >= (*m_cache)->GetPkgPriority(pkg)))
                     {
                         is_obsoleted = true;
                         break;
