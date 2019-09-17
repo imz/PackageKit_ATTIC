16304ed34ab902fdaac9952cae61811d3cbd666f
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-09-17 14:34:15 +0300

    Aptcc: add lua scripts support
    (cherry picked from commit b24014deddd4f198e77d6d8e9662ef670ce87229)

--- backends/apt/apt-job.cpp
+++ backends/apt/apt-job.cpp
@@ -33,6 +33,10 @@
 #include <apt-pkg/pkgsystem.h>
 #include <apt-pkg/version.h>
 
+#ifdef WITH_LUA
+#include <apt-pkg/luaiface.h>
+#endif
+
 #include <appstream.h>
 
 #include <sys/prctl.h>
@@ -1866,6 +1870,12 @@ bool AptJob::installPackages(PkBitfield flags)
     if (m_cache->isRemovingEssentialPackages()) {
         return false;
     }
+
+#ifdef WITH_LUA
+    _lua->SetDepCache(*m_cache);
+    _lua->RunScripts("Scripts::PackageKit::RunTransaction::Pre");
+    _lua->ResetCaches();
+#endif
 
     // Sanity check
     if ((*m_cache)->BrokenCount() != 0) {
