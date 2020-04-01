08b392031a6ea54406f47a2865feb713decf384a
Aleksei Nikiforov <darktemplar@altlinux.org>
2020-04-01 16:05:07 +0300

    Revert "aptcc: Implement frontend-locking"
    This reverts commit 5ba21995f9a212076bc8957d1ef1be7110ec1db4.
    
    (cherry picked from commit a75a9dd1ec90c0deb2b872771f202abb41ebea59)

--- backends/apt/apt-job.cpp
+++ backends/apt/apt-job.cpp
@@ -2389,7 +2389,7 @@ bool AptJob::installPackages(PkBitfield flags)
 
     // we could try to see if this is the case
     g_setenv("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", TRUE);
-    _system->UnLockInner();
+    _system->UnLock();
 
     pkgPackageManager::OrderResult res;
     res = PM->DoInstallPreFork();
@@ -2510,7 +2510,6 @@ bool AptJob::installPackages(PkBitfield flags)
     close(readFromChildFD[0]);
     close(readFromChildFD[1]);
     close(pty_master);
-    _system->LockInner();
 
     g_debug("apt-backend parent process finished: %d", ret);
 
