d660c0c6ee49126429bc8cb54407070536a72a27
Aleksei Nikiforov <darktemplar@altlinux.org>
2020-10-27 18:38:07 +0300

    Cut functions depending on access to rpm via apt
    On request by rider@
    
    (cherry picked from commit 6db26c8e5c168af20a5307ce953ed9ef1a2ca6ca)

--- backends/aptcc/apt-intf.h
+++ backends/aptcc/apt-intf.h
@@ -128,11 +128,6 @@ public:
     PkgList searchPackageDetails(const vector<string> &queries);
 
     /**
-      * Returns a list of all packages that matched contains the given files
-      */
-    PkgList searchPackageFiles(gchar **values);
-
-    /**
       * Returns a list of all packages that can be updated
       * Pass a PkgList to get the blocked updates as well
       */
@@ -194,11 +189,6 @@ public:
     void emitUpdateDetails(const PkgList &pkgs);
 
     /**
-      *  Emits the files of a package
-      */
-    void emitPackageFiles(const gchar *pi);
-
-    /**
       *  Download and install packages
       */
     bool installPackages(PkBitfield flags);
@@ -229,7 +219,6 @@ public:
 private:
     void setEnvLocaleFromJob();
     bool packageIsSupported(const pkgCache::VerIterator &verIter, string component);
-    bool isApplication(const pkgCache::VerIterator &verIter);
     bool matchesQueries(const vector<string> &queries, string s);
 
     /**
