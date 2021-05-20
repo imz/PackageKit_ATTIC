62188573f25430225fd9f69b3cfe761e6a51da69
Ivan Zakharyaschev <imz@altlinux.org>
2021-05-20 13:21:18 +0300

    aptcc: mark methods as override to avoid coding errors when API changes
    This helps to avoid errors when the library API (and/or some base
    classes) change.
    
    The earlier we mark methods override, the easier it will be to catch
    dependent changes required after API changes in future.
    
    The "override" mark (keyword) is available since C++11.

--- backends/aptcc/apt-cache-file.h
+++ backends/aptcc/apt-cache-file.h
@@ -166,10 +166,10 @@ public:
     OpPackageKitProgress(PkBackendJob *job);
     virtual ~OpPackageKitProgress();
 
-    virtual void Done();
+    virtual void Done() override;
 
 protected:
-    virtual void Update();
+    virtual void Update() override;
 
 private:
     PkBackendJob  *m_job;
