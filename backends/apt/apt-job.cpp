4c2fbf4894ac58532b557eb7287d4bc52b138e5e
Aleksei Nikiforov <darktemplar@altlinux.org>
2022-01-27 13:43:10 +0300

    Force cache to reload all files
    (cherry picked from commit 7ff67b44a5fbbf0045f902108f5512e912c0e382)

--- backends/apt/apt-job.cpp
+++ backends/apt/apt-job.cpp
@@ -63,7 +63,6 @@
 #define RAMFS_MAGIC     0x858458f6
 
 AptJob::AptJob(PkBackendJob *job) :
-    m_cache(nullptr),
     m_job(job),
     m_cancel(false),
     m_lastSubProgress(0),
@@ -89,11 +88,6 @@ AptJob::AptJob(PkBackendJob *job) :
         g_autofree gchar *uri = pk_backend_convert_uri(ftp_proxy);
         g_setenv("ftp_proxy", uri, TRUE);
     }
-}
-
-AptJob::~AptJob()
-{
-    delete m_cache;
 }
 
 bool AptJob::init(gchar **localDebs)
@@ -151,7 +145,7 @@ bool AptJob::init(gchar **localDebs)
     }
 
     // Create the AptCacheFile class to search for packages
-    m_cache = new AptCacheFile(m_job, withLock, &m_progress);
+    m_cache.reset(new AptCacheFile(m_job, withLock, &m_progress));
     while (m_cache->Open() == false) {
         if (withLock == false || (timeout <= 0)) {
             show_errors(m_job, PK_ERROR_ENUM_CANNOT_GET_LOCK);
@@ -166,8 +160,7 @@ bool AptJob::init(gchar **localDebs)
         // If we are going to try again, we can either simply try Open() once
         // again (since pkgCacheFile is monotonic in creating required objects),
         // or simply continue with a new pkgCacheFile object.
-        delete m_cache;
-        m_cache = new AptCacheFile(m_job, withLock, &m_progress);
+        m_cache.reset(new AptCacheFile(m_job, withLock, &m_progress));
     }
 
     // default settings
@@ -691,7 +684,7 @@ bool AptJob::getArchive(pkgAcquire *Owner,
 
 AptCacheFile* AptJob::aptCacheFile() const
 {
-    return m_cache;
+    return m_cache.get();
 }
 
 // used to emit packages it collects all the needed info
@@ -1443,6 +1436,17 @@ void AptJob::refreshCache()
 {
     pk_backend_job_set_status(m_job, PK_STATUS_ENUM_REFRESH_CACHE);
 
+    // Rebuild the cache.
+
+    // First, destroy m_cache which holds the old copy of the pkgCache and
+    // dependent objects. Note that BuildCaches() alone won't overwrite the held
+    // pkgCache if it's already computed (per Debian's current APT API).
+    m_cache.reset();
+
+    pkgCacheFile::RemoveCaches();
+
+    m_cache.reset(new AptCacheFile(m_job, true /* withLock */, &m_progress));
+
     if (m_cache->BuildSourceList() == false) {
         return;
     }
@@ -1453,17 +1457,6 @@ void AptJob::refreshCache()
     // do the work
     ListUpdate(Stat, *m_cache->GetSourceList(), *m_cache);
 
-    // Rebuild the cache.
-
-    // First, destroy m_cache which holds the old copy of the pkgCache and
-    // dependent objects. Note that BuildCaches() alone won't overwrite the held
-    // pkgCache if it's already computed (per Debian's current APT API).
-    delete m_cache;
-    m_cache = nullptr;
-
-    pkgCacheFile::RemoveCaches();
-
-    m_cache = new AptCacheFile(m_job, true /* withLock */, &m_progress);
     // Setting WithLock implies not AllowMem in APT. (Building in memory only
     // would be a waste of time for refreshCache(): nothing saved to disk.)
     // So does apt-get update, too.
