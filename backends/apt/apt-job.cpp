1d496d61819d9d4d8cc2f232a7be5dbf44db4125
Ivan Zakharyaschev <imz@altlinux.org>
2023-11-20 13:13:45 +0300

    AptIntf::{search,emit}PackageFiles: rewrite with SCOPE_EXIT from boost
    (The insertion of an extra newline is done for cleaner patching
    in the next changes.)
    
    Extracted the part that makes sense for the upstream Debian-specific code
    from the commits (to present those changes in a simpler way):
    
    Implement searchPackageFiles (adaped for ALT/rpm)
    
    (cherry picked from commit ee5914e5926c005f1db2cea81241ae30191d157b)
    
    Fixes for emitPackageFiles
    
    (cherry picked from commit daec8866103a10f6d8e975e0eb4eb48242124164)

--- backends/apt/apt-job.cpp
+++ backends/apt/apt-job.cpp
@@ -37,6 +37,8 @@
 
 #include <appstream.h>
 
+#include <boost/scope_exit.hpp>
+
 #include <sys/prctl.h>
 #include <sys/statvfs.h>
 #include <sys/statfs.h>
@@ -1275,11 +1277,15 @@ PkgList AptJob::searchPackageFiles(gchar **values)
         return output;
     }
 
+    BOOST_SCOPE_EXIT( (&re) )
+    {
+        regfree(&re);
+    } BOOST_SCOPE_EXIT_END;
+
     DIR *dp;
     struct dirent *dirp;
     if (!(dp = opendir("/var/lib/dpkg/info/"))) {
         g_debug ("Error opening /var/lib/dpkg/info/\n");
-        regfree(&re);
         return output;
     }
 
@@ -1307,7 +1313,6 @@ PkgList AptJob::searchPackageFiles(gchar **values)
         }
     }
     closedir(dp);
-    regfree(&re);
 
     // Resolve the package names now
     for (const string &name : packages) {
@@ -1516,6 +1521,7 @@ void AptJob::emitPackageFiles(const gchar *pi)
     string line;
 
     g_auto(GStrv) parts = pk_package_id_split(pi);
+
     string fName;
     fName = "/var/lib/dpkg/info/" +
             string(parts[PK_PACKAGE_ID_NAME]) +
@@ -1536,6 +1542,12 @@ void AptJob::emitPackageFiles(const gchar *pi)
         }
 
         files = g_ptr_array_new_with_free_func(g_free);
+
+        BOOST_SCOPE_EXIT( (&files) )
+        {
+            g_ptr_array_unref(files);
+        } BOOST_SCOPE_EXIT_END;
+
         while (in.eof() == false) {
             getline(in, line);
             if (!line.empty()) {
@@ -1547,7 +1559,6 @@ void AptJob::emitPackageFiles(const gchar *pi)
             g_ptr_array_add(files, NULL);
             pk_backend_job_files(m_job, pi, (gchar **) files->pdata);
         }
-        g_ptr_array_unref(files);
     }
 }
 
