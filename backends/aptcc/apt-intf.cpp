e3355ead90a9d59244c4c2198e1bafa6787be496
Ivan Zakharyaschev <imz@altlinux.org>
2023-11-20 13:13:45 +0300

    funcs for file lists (of a package): rewrite with SCOPE_EXIT from boost
    Extracted the part that makes sense for the upstream Debian-specific code
    from the commits (to present those changes in a simpler way):
    
    Implement searchPackageFiles (adaped for ALT/rpm)
    
    (cherry picked from commit ee5914e5926c005f1db2cea81241ae30191d157b)
    
    Fixes for emitPackageFiles
    
    (cherry picked from commit daec8866103a10f6d8e975e0eb4eb48242124164)

--- backends/aptcc/apt-intf.cpp
+++ backends/aptcc/apt-intf.cpp
@@ -37,6 +37,8 @@
 
 #include <appstream.h>
 
+#include <boost/scope_exit.hpp>
+
 #include <sys/statvfs.h>
 #include <sys/statfs.h>
 #include <sys/wait.h>
@@ -1235,11 +1237,15 @@ PkgList AptIntf::searchPackageFiles(gchar **values)
         return output;
     }
 
+    BOOST_SCOPE_EXIT( (&re) )
+    {
+        regfree(&re);
+    } BOOST_SCOPE_EXIT_END;
+
     DIR *dp;
     struct dirent *dirp;
     if (!(dp = opendir("/var/lib/dpkg/info/"))) {
         g_debug ("Error opening /var/lib/dpkg/info/\n");
-        regfree(&re);
         return output;
     }
 
@@ -1267,7 +1273,6 @@ PkgList AptIntf::searchPackageFiles(gchar **values)
         }
     }
     closedir(dp);
-    regfree(&re);
 
     // Resolve the package names now
     for (const string &name : packages) {
@@ -1503,6 +1508,12 @@ void AptIntf::emitPackageFiles(const gchar *pi)
         }
 
         files = g_ptr_array_new_with_free_func(g_free);
+
+        BOOST_SCOPE_EXIT( (&files) )
+        {
+            g_ptr_array_unref(files);
+        } BOOST_SCOPE_EXIT_END;
+
         while (in.eof() == false) {
             getline(in, line);
             if (!line.empty()) {
@@ -1514,7 +1525,6 @@ void AptIntf::emitPackageFiles(const gchar *pi)
             g_ptr_array_add(files, NULL);
             pk_backend_job_files(m_job, pi, (gchar **) files->pdata);
         }
-        g_ptr_array_unref(files);
     }
 }
 
