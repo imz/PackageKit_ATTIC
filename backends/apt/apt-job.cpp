c50b337d29f3745bbb0d930455645e10363b3b75
Ivan Zakharyaschev <imz@altlinux.org>
2023-11-21 19:32:42 +0300

    updateInterface: don't track whether already errorEmitted (non-significant in ALT)
    The whole updateInterface func is not going to be used in ALT at all.
    (This brings the code closer to the previous version so that further
    patches can be applied easier.)
    
    The errorEmitted var was used to pass information from
    updateInterface() to the caller about whether an error was already
    signalled to the UI or the caller has to do it itself. (AFAIU)
    
    This reverts the essence of PACKAGEKIT_1_2_5~3:
    15fe84f04 (aptcc: Emit only the first, likely highest-quality error when dpkg fails, 2022-02-17)
    
    The whole updateInterface func will be dropped altogether in 1.2.5.0.0.30-alt1~4:
    facf631cc (drop unused AptIntf::updateInterface() (employed for parsing dpkg output), 2023-07-05)
    
    Follows 1.1.12-alt1~3 (i.e., makes the code similar to what was
    after that patch in ALT):
    4f162d329 (Remove debconf support, 2018-06-15)

--- backends/apt/apt-job.cpp
+++ backends/apt/apt-job.cpp
@@ -1372,7 +1372,7 @@ pkgCache::VerIterator AptJob::findTransactionPackage(const std::string &name)
     return candidateVer;
 }
 
-void AptJob::updateInterface(int fd, int writeFd, bool *errorEmitted)
+void AptJob::updateInterface(int fd, int writeFd)
 {
     char buf[2];
     static char line[1024] = "";
@@ -1422,8 +1422,6 @@ void AptJob::updateInterface(int fd, int writeFd, bool *errorEmitted)
                                           PK_ERROR_ENUM_PACKAGE_FAILED_TO_INSTALL,
                                           "Error while installing package: %s",
                                           str.c_str());
-                if (errorEmitted != nullptr)
-                    *errorEmitted = true;
             } else if (strstr(status, "pmstatus") != NULL) {
                 // INSTALL & UPDATE
                 // - Running dpkg
@@ -2032,7 +2030,6 @@ bool AptJob::installPackages(PkBitfield flags)
     int ret = 0;
     char masterbuf[1024];
     std::string errorLogTail = "";
-    bool errorEmitted = false;
     bool childTerminated = false;
     while (true) {
         while (true) {
@@ -2050,7 +2047,7 @@ bool AptJob::installPackages(PkBitfield flags)
             break;
 
         // try to parse dpkg status
-        updateInterface(readFromChildFD[0], pty_master, &errorEmitted);
+        updateInterface(readFromChildFD[0], pty_master);
 
         // Check if the child died
         if (waitpid(m_child_pid, &ret, WNOHANG) != 0)
@@ -2063,7 +2060,7 @@ bool AptJob::installPackages(PkBitfield flags)
 
     cout << "apt-backend parent process finished: " << ret << endl;
 
-    if (ret != 0 && !m_cancel && !errorEmitted) {
+    if (ret != 0 && !m_cancel) {
         // If the child died with a non-zero exit code, and we didn't deliberately
         // kill it in a cancel operation and we didn't already emit an error,
         // we still need to find out what went wrong to present a message to the user.
